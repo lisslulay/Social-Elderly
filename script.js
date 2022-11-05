749cbb2b3573e54a00f7d639a2c418175b4d7d13
liss <liss.s.li@outlook.com>
2022-11-06 03:20:11 +0800

    find people

--- script.js
+++ script.js
@@ -1,23 +1,75 @@
-$(function() {
-    var createPost = $("#create-post-button");
-    createPost.on({
-        click: function() {
-            $("#create-post-box").show();
-        }
-    });
+function disableScrollbars() {
+  $("body").addClass("disable-scrollbars");
+}
+function enableScrollbars() {
+  $("body").removeClass("disable-scrollbars");
+}
 
-    var closeOverlay = $(".close");
-    closeOverlay.on({
-        click: function() {
-            $(this).parent().parent().parent().hide();
-        }
-    });
+$(function () {
+  var closeOverlay = $(".close");
+  closeOverlay.on({
+    click: function () {
+      $(this).parent().parent().parent().hide();
+      enableScrollbars();
+    },
+  });
 
-    var sendPost = $("#send-post");
-    sendPost.on({
-        click: function() {
-            $(this).parent().parent().hide();
-            $(".send-successfully").show();
-        }
-    });
+  /* ===============================post=========================== */
+  var createPost = $("#create-post-button");
+  createPost.on({
+    click: function () {
+      $("#create-post-box").show();
+      disableScrollbars();
+    },
+  });
+
+  var sendPost = $("#send-post");
+  sendPost.on({
+    click: function () {
+      $("#create-post-box").hide();
+      disableScrollbars();
+      $(".send-successfully").show();
+    },
+  });
+
+  /* ===============================friends=========================== */
+  var videoButton = $("#find-friends-button");
+  var exploreButton = $("#find-arounds-button");
+
+  videoButton.on({
+    click: function () {
+      $("#invite-friends").show();
+      disableScrollbars();
+    },
+  });
+
+  exploreButton.on({
+    click: function () {
+      $("#invite-arounds").show();
+      disableScrollbars();
+    },
+  });
+
+  var inviteButton = $(
+    "#invite-friends .friends-entry>.button, #invite-arounds .friends-entry>.button"
+  );
+  inviteButton.on({
+    click: function () {
+      $(this).toggleClass("active");
+      var atext = $(this).text();
+      if (atext == "invite") {
+        $(this).text("invited");
+      } else {
+        $(this).text("invite");
+      }
+    },
+  });
+
+  var addFriendButton = $(".video-box-bottom>a");
+  addFriendButton.on({
+    click: function() {
+        $(".add-friends-successfully").show();
+        disableScrollbars();
+    }
+  });
 });
